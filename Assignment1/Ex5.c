09b2d3e1a2e52a05b0212e593d3368e6bb8f65c1
Salah Bendary <salahbendary1@gmail.com>
2023-11-19 19:26:42 +0200

    Create Ex5.c

---
+++ Assignment1/Ex5.c
@@ -0,0 +1,19 @@
+///**************************************************************************************************
+// Name        : ex5.c
+// Author      : Salah BENDARY
+// Description : Assignment 1 - Ex 5
+//Write a program to print the ASCII value of a character
+//input by the user.
+// **************************************************************************************************/
+//
+//#include <stdio.h>
+//
+//int main () {
+//	setvbuf(stdout, NULL, _IONBF, 0);
+//	setvbuf(stderr, NULL, _IONBF, 0);
+//	unsigned char u8character = 'a';
+//	printf("Enter any character to take it ASCII:\t");
+//	scanf("%d",&u8character);
+//	printf("The ASCII value of %c is %d\n",u8character,u8character);
+//	return 0 ;
+//}
