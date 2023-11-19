b82deffba9e481f9e98d58dcb408a6992ae3d4fb
Salah Bendary <salahbendary1@gmail.com>
2023-11-19 19:26:32 +0200

    Create Ex4.c

---
+++ Assignment1/Ex4.c
@@ -0,0 +1,24 @@
+///**************************************************************************************************
+// Name        : ex4.c
+// Author      : Salah BENDARY
+// Description : Assignment 1 - Ex 4
+//Write a program that reads the radius of a circle and
+//calculates the area and circumference then prints the results.
+// **************************************************************************************************/
+//
+//#define PI 3.14
+//#include <stdio.h>
+//
+//int main () {
+//	setvbuf(stdout, NULL, _IONBF, 0);
+//	setvbuf(stderr, NULL, _IONBF, 0);
+//	unsigned int u32circuleRadius = 0;
+//	unsigned int u32circuleArea = 0;
+//	unsigned int u32circuleCircumference = 0;
+//	printf("Enter the Radius of the circle:\t");
+//	scanf("%d",&u32circuleRadius);
+//	u32circuleArea = PI*u32circuleRadius*u32circuleRadius;
+//	u32circuleCircumference = (PI*(u32circuleRadius+u32circuleRadius));
+//	printf("The area of the circle: %d\nAnd the circumference is: %d\n",u32circuleArea,u32circuleCircumference);
+//	return 0 ;
+//}
