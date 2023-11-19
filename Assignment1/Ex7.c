07bcfe5988c99e2dc431dbae058f54c021cf3afe
Salah Bendary <salahbendary1@gmail.com>
2023-11-19 23:28:05 +0200

    Update Ex7.c

--- Assignment1/Ex7.c
+++ Assignment1/Ex7.c
@@ -5,27 +5,27 @@
 //Write a program that takes three integers, and prints out the smallest number
 // **************************************************************************************************/
 //
-//#include <stdio.h>
-//
-//int main () {
-//	setvbuf(stdout, NULL, _IONBF, 0);
-//	setvbuf(stderr, NULL, _IONBF, 0);
-//	unsigned char u8numberOne = 0;
-//	unsigned char u8numberTwo = 0;
-//	unsigned char u8numberThree = 0;
-//	printf("Enter Three numbers:\t");
-//	scanf("%d %d %d",&u8numberOne,&u8numberTwo,&u8numberThree);
-//	if((u8numberOne<u8numberTwo) && (u8numberOne<u8numberThree))
-//	{
-//		printf("The smallest number is %d",u8numberOne);
-//	}
-//	else if ((u8numberTwo < u8numberOne ) && (u8numberTwo<u8numberThree))
-//		{
-//			printf("The smallest number is %d",u8numberTwo);
-//		}
-//
-//	else { printf("The smallest number is %d",u8numberThree);
-//	}
-//
-//	return 0 ;
-//}
+#include <stdio.h>
+
+int main () {
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+	unsigned char u8numberOne = 0;
+	unsigned char u8numberTwo = 0;
+	unsigned char u8numberThree = 0;
+	printf("Enter Three numbers:\t");
+	scanf("%d %d %d",&u8numberOne,&u8numberTwo,&u8numberThree);
+	if((u8numberOne<u8numberTwo) && (u8numberOne<u8numberThree))
+	{
+		printf("The smallest number is %d",u8numberOne);
+	}
+	else if ((u8numberTwo < u8numberOne ) && (u8numberTwo<u8numberThree))
+		{
+			printf("The smallest number is %d",u8numberTwo);
+		}
+
+	else { printf("The smallest number is %d",u8numberThree);
+	}
+
+	return 0 ;
+}
