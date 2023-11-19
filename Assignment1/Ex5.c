4ff2c4c4abb9e98d944a94742824653355351597
Salah Bendary <salahbendary1@gmail.com>
2023-11-19 23:28:14 +0200

    Update Ex5.c

--- Assignment1/Ex5.c
+++ Assignment1/Ex5.c
@@ -6,14 +6,14 @@
 //input by the user.
 // **************************************************************************************************/
 //
-//#include <stdio.h>
-//
-//int main () {
-//	setvbuf(stdout, NULL, _IONBF, 0);
-//	setvbuf(stderr, NULL, _IONBF, 0);
-//	unsigned char u8character = 'a';
-//	printf("Enter any character to take it ASCII:\t");
-//	scanf("%d",&u8character);
-//	printf("The ASCII value of %c is %d\n",u8character,u8character);
-//	return 0 ;
-//}
+#include <stdio.h>
+
+int main () {
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+	unsigned char u8character = 'a';
+	printf("Enter any character to take it ASCII:\t");
+	scanf("%d",&u8character);
+	printf("The ASCII value of %c is %d\n",u8character,u8character);
+	return 0 ;
+}
