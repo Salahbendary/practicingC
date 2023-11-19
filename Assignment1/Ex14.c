973d78afd5ea1d485ff973e0d9231ed097255fe7
Salah Bendary <salahbendary1@gmail.com>
2023-11-19 23:28:27 +0200

    Update Ex14.c

--- Assignment1/Ex14.c
+++ Assignment1/Ex14.c
@@ -5,14 +5,14 @@
 //Wrrite a program to display English alphabets from A to Z.
 // **************************************************************************************************/
 //
-//#include <stdio.h>
-//
-//int main () {
-//    setvbuf(stdout, NULL, _IONBF, 0);
-//    setvbuf(stderr, NULL, _IONBF, 0);
-//    unsigned char u8counter = 0;
-//    for(u8counter = 'A';u8counter<='Z';u8counter++) {
-//    	printf("%c ",u8counter);
-//    }
-//    return 0;
-//    }
+#include <stdio.h>
+
+int main () {
+ setvbuf(stdout, NULL, _IONBF, 0);
+ setvbuf(stderr, NULL, _IONBF, 0);
+ unsigned char u8counter = 0;
+ for(u8counter = 'A';u8counter<='Z';u8counter++) {
+ 	printf("%c ",u8counter);
+ }
+ return 0;
+ }
