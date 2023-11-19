e95cf9be5dc154e91bf2100462629483f942145a
Salah Bendary <salahbendary1@gmail.com>
2023-11-19 19:26:29 +0200

    Create Ex2.c

---
+++ Assignment1/Ex2.c
@@ -0,0 +1,20 @@
+///**************************************************************************************************
+// Name        : ex2.c
+// Author      : Salah BENDARY
+// Description : Assignment 1 - Ex 2
+//Write a program that print your name and your grade
+//in a new line.
+// **************************************************************************************************/
+//
+//#include <stdio.h>
+//
+//int main () {
+//	char u8myName[20] = "Salah BENDARY";
+//	int u32grade = 95;
+//	char u8myGrade[10] = "Excellent";
+//
+//	setvbuf(stdout, NULL, _IONBF, 0);
+//	setvbuf(stderr, NULL, _IONBF, 0);
+//	printf("My name is %s\nMy Grade is %d\n%s",u8myName,u32grade,u8myGrade);
+//	return 0 ;
+//}
