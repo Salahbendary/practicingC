35cb007dc84fb43096d8d06e4dcee84b515f9475
Salah Bendary <salahbendary1@gmail.com>
2023-11-19 19:26:34 +0200

    Create Ex3.c

---
+++ Assignment1/Ex3.c
@@ -0,0 +1,20 @@
+///**************************************************************************************************
+// Name        : ex3.c
+// Author      : Salah BENDARY
+// Description : Assignment 1 - Ex 3
+//Write a program for converting temperature from degrees Celsius to degrees Fahrenheit,
+//given the formula:
+//F = C * 9/5 + 32
+// **************************************************************************************************/
+//
+//#include <stdio.h>
+//
+//int main () {
+//	setvbuf(stdout, NULL, _IONBF, 0);
+//	setvbuf(stderr, NULL, _IONBF, 0);
+//	unsigned int u32tempCelsius = 37;
+//	unsigned int u32tempFahrenheit = 0;
+//	u32tempFahrenheit = ((u32tempCelsius * (9/5)) + 32);
+//	printf("The Fahrenheit temperature is: %d \n",u32tempFahrenheit);
+//	return 0 ;
+//}
