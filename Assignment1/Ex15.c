2f2ee8bdbdd06614c6c3bec7a07c892bdec1f353
Salah Bendary <salahbendary1@gmail.com>
2023-11-19 23:28:32 +0200

    Update Ex15.c

--- Assignment1/Ex15.c
+++ Assignment1/Ex15.c
@@ -6,21 +6,21 @@
 // The number and its power are input from user
 // **************************************************************************************************/
 //
-//#include <stdio.h>
-//
-//int main () {
-//    setvbuf(stdout, NULL, _IONBF, 0);
-//    setvbuf(stderr, NULL, _IONBF, 0);
-//    unsigned int u16number;
-//    unsigned int u16powerOfNumber;
-//    unsigned int u32result =1;
-//    printf("Enter the number and its power:");
-//    scanf("%d %d",&u16number,&u16powerOfNumber);
-//    printf("%d ^ %d =",u16number,u16powerOfNumber);
-//    while(u16powerOfNumber!=0) {
-//    	u32result*=u16number;
-//    	u16powerOfNumber--;
-//    }
-//    printf(" %d",u32result);
-//    return 0;
-//    }
+#include <stdio.h>
+
+int main () {
+setvbuf(stdout, NULL, _IONBF, 0);
+setvbuf(stderr, NULL, _IONBF, 0);
+unsigned int u16number;
+unsigned int u16powerOfNumber;
+unsigned int u32result =1;
+printf("Enter the number and its power:");
+scanf("%d %d",&u16number,&u16powerOfNumber);
+printf("%d ^ %d =",u16number,u16powerOfNumber);
+while(u16powerOfNumber!=0) {
+	u32result*=u16number;
+	u16powerOfNumber--;
+}
+printf(" %d",u32result);
+return 0;
+}
