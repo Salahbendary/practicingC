e804f7711d15180129a4312436e2242cb5db7b5f
Salah Bendary <salahbendary1@gmail.com>
2023-11-19 23:28:30 +0200

    Update Ex2.c

--- Assignment1/Ex2.c
+++ Assignment1/Ex2.c
@@ -6,15 +6,15 @@
 //in a new line.
 // **************************************************************************************************/
 //
-//#include <stdio.h>
-//
-//int main () {
-//	char u8myName[20] = "Salah BENDARY";
-//	int u32grade = 95;
-//	char u8myGrade[10] = "Excellent";
-//
-//	setvbuf(stdout, NULL, _IONBF, 0);
-//	setvbuf(stderr, NULL, _IONBF, 0);
-//	printf("My name is %s\nMy Grade is %d\n%s",u8myName,u32grade,u8myGrade);
-//	return 0 ;
-//}
+#include <stdio.h>
+
+int main () {
+	char u8myName[20] = "Salah BENDARY";
+	int u32grade = 95;
+	char u8myGrade[10] = "Excellent";
+
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+	printf("My name is %s\nMy Grade is %d\n%s",u8myName,u32grade,u8myGrade);
+	return 0 ;
+}
