43e15aa676f1edce7ebd950e7a0b6c4cfafa2b00
Salah Bendary <salahbendary1@gmail.com>
2023-11-19 19:26:16 +0200

    Create Ex1.c

---
+++ Assignment1/Ex1.c
@@ -0,0 +1,21 @@
+///**************************************************************************************************
+// Name        : ex1.c
+// Author      : Salah BENDARY
+// Description : Assignment 1 - Ex 1
+// Write a program that take two integers from the user and print the results of this equation:
+//				Result = ((num1 + num2) * 3) – 10
+// **************************************************************************************************/
+//
+//#include <stdio.h>
+//
+//int main () {
+//	int u8numberOne = 0;
+//	int u8numberTwo = 0;
+//	setvbuf(stdout, NULL, _IONBF, 0);
+//	setvbuf(stderr, NULL, _IONBF, 0);
+//	printf("Enter the two numbers sequentially:\n");
+//	scanf("%d %d",&u8numberOne,&u8numberTwo);
+//	printf("%d",(((u8numberOne+u8numberTwo)*3)-10));
+//
+//	return 0 ;
+//}
