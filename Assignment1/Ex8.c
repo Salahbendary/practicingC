73ade60424afcf8505f742337ec0b20949b267cb
Salah Bendary <salahbendary1@gmail.com>
2023-11-19 23:28:02 +0200

    Update Ex8.c

--- Assignment1/Ex8.c
+++ Assignment1/Ex8.c
@@ -5,22 +5,22 @@
 //Write a program that reads a positive integer and checks if it is a perfect square.
 // **************************************************************************************************/
 //
-//#include <stdio.h>
-//
-//int main () {
-//	setvbuf(stdout, NULL, _IONBF, 0);
-//	setvbuf(stderr, NULL, _IONBF, 0);
-//	unsigned char u8isPerfectSquare = 0; /* input for Positive Integer*/
-//	unsigned char u8number = 1; /* Number to check if*/
-//	printf("Enter the number:\t");
-//	scanf("%d",&u8isPerfectSquare);
-//	while(u8number*u8number<u8isPerfectSquare) {
-//		u8number++; }
-//
-//	if(u8number*u8number==u8isPerfectSquare) {
-//			printf("is perfect square");
-//		}
-//	else { printf("not perfect square");
-//	}
-//	return 0 ;
-//}
+#include <stdio.h>
+
+int main () {
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+	unsigned char u8isPerfectSquare = 0; /* input for Positive Integer*/
+	unsigned char u8number = 1; /* Number to check if*/
+	printf("Enter the number:\t");
+	scanf("%d",&u8isPerfectSquare);
+	while(u8number*u8number<u8isPerfectSquare) {
+		u8number++; }
+
+	if(u8number*u8number==u8isPerfectSquare) {
+			printf("is perfect square");
+		}
+	else { printf("not perfect square");
+	}
+	return 0 ;
+}
