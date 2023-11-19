8f8d2863c42c1038342958148ec8cb9ba00c7963
Salah Bendary <salahbendary1@gmail.com>
2023-11-19 19:26:39 +0200

    Create Ex6.c

---
+++ Assignment1/Ex6.c
@@ -0,0 +1,30 @@
+///**************************************************************************************************
+// Name        : ex6.c
+// Author      : Salah BENDARY
+// Description : Assignment 1 - Ex 6
+//Write a program that print the relation between two integer number
+// if those numbers are equal, not equal and which one contain the higher value
+// **************************************************************************************************/
+//
+//#include <stdio.h>
+//
+//int main () {
+//	setvbuf(stdout, NULL, _IONBF, 0);
+//	setvbuf(stderr, NULL, _IONBF, 0);
+//	unsigned int u32numberOne = 0;
+//	unsigned int u32numberTwo = 0;
+//	printf("Enter two numbers:\t");
+//	scanf("%d %d",&u32numberOne,&u32numberTwo);
+//	if (u32numberOne == u32numberTwo) {
+//		printf("The relation between %d and %d is Equal\n",u32numberOne,u32numberTwo);
+//	}
+//	else if (u32numberOne > u32numberTwo) {
+//		printf("The first Number %d > Second Number %d\n",u32numberOne,u32numberTwo);
+//	}
+//	else if (u32numberOne < u32numberTwo) {
+//
+//		printf("The first Number %d < Second Number %d\n",u32numberOne,u32numberTwo);
+//	}
+//
+//	return 0 ;
+//}
