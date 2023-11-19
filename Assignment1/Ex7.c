8e24446685ee4eafb4b6d806fcbae9c5963b43de
Salah Bendary <salahbendary1@gmail.com>
2023-11-19 19:26:46 +0200

    Create Ex7.c

---
+++ Assignment1/Ex7.c
@@ -0,0 +1,31 @@
+///**************************************************************************************************
+// Name        : ex7.c
+// Author      : Salah BENDARY
+// Description : Assignment 1 - Ex 7
+//Write a program that takes three integers, and prints out the smallest number
+// **************************************************************************************************/
+//
+//#include <stdio.h>
+//
+//int main () {
+//	setvbuf(stdout, NULL, _IONBF, 0);
+//	setvbuf(stderr, NULL, _IONBF, 0);
+//	unsigned char u8numberOne = 0;
+//	unsigned char u8numberTwo = 0;
+//	unsigned char u8numberThree = 0;
+//	printf("Enter Three numbers:\t");
+//	scanf("%d %d %d",&u8numberOne,&u8numberTwo,&u8numberThree);
+//	if((u8numberOne<u8numberTwo) && (u8numberOne<u8numberThree))
+//	{
+//		printf("The smallest number is %d",u8numberOne);
+//	}
+//	else if ((u8numberTwo < u8numberOne ) && (u8numberTwo<u8numberThree))
+//		{
+//			printf("The smallest number is %d",u8numberTwo);
+//		}
+//
+//	else { printf("The smallest number is %d",u8numberThree);
+//	}
+//
+//	return 0 ;
+//}
