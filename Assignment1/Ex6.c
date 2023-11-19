bd2871848fc36b3ae0328eea994ec0e5f5a21b5e
Salah Bendary <salahbendary1@gmail.com>
2023-11-19 23:28:07 +0200

    Update Ex6.c

--- Assignment1/Ex6.c
+++ Assignment1/Ex6.c
@@ -8,23 +8,23 @@
 //
 //#include <stdio.h>
 //
-//int main () {
-//	setvbuf(stdout, NULL, _IONBF, 0);
-//	setvbuf(stderr, NULL, _IONBF, 0);
-//	unsigned int u32numberOne = 0;
-//	unsigned int u32numberTwo = 0;
-//	printf("Enter two numbers:\t");
-//	scanf("%d %d",&u32numberOne,&u32numberTwo);
-//	if (u32numberOne == u32numberTwo) {
-//		printf("The relation between %d and %d is Equal\n",u32numberOne,u32numberTwo);
-//	}
-//	else if (u32numberOne > u32numberTwo) {
-//		printf("The first Number %d > Second Number %d\n",u32numberOne,u32numberTwo);
-//	}
-//	else if (u32numberOne < u32numberTwo) {
-//
-//		printf("The first Number %d < Second Number %d\n",u32numberOne,u32numberTwo);
-//	}
-//
-//	return 0 ;
-//}
+int main () {
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+	unsigned int u32numberOne = 0;
+	unsigned int u32numberTwo = 0;
+	printf("Enter two numbers:\t");
+	scanf("%d %d",&u32numberOne,&u32numberTwo);
+	if (u32numberOne == u32numberTwo) {
+		printf("The relation between %d and %d is Equal\n",u32numberOne,u32numberTwo);
+	}
+	else if (u32numberOne > u32numberTwo) {
+		printf("The first Number %d > Second Number %d\n",u32numberOne,u32numberTwo);
+	}
+	else if (u32numberOne < u32numberTwo) {
+
+		printf("The first Number %d < Second Number %d\n",u32numberOne,u32numberTwo);
+	}
+
+	return 0 ;
+}
