10d3a9268fe5f7ef80db09c902c8a6d4e36a0646
Salah Bendary <salahbendary1@gmail.com>
2023-11-19 19:26:13 +0200

    Create Ex12.c

---
+++ Assignment1/Ex12.c
@@ -0,0 +1,27 @@
+///**************************************************************************************************
+// Name        : ex12.c
+// Author      : Salah BENDARY
+// Description : Assignment 1 - Ex 12
+//Write a program that reads a positive integer and computes the factorial.
+// **************************************************************************************************/
+//
+//#include <stdio.h>
+//
+//int main () {
+//    setvbuf(stdout, NULL, _IONBF, 0);
+//    setvbuf(stderr, NULL, _IONBF, 0);
+//    unsigned int u8number = 1; /* Number to get its factorial*/
+//    unsigned int u8factorial = 1; /* Number to store the result*/
+//    printf("Enter the number:\t");
+//    scanf("%d",&u8number);
+//
+//    printf("Factorial of %d is:", u8number);  // Display the original number
+//
+//    while(u8number != 0) {
+//        u8factorial *= u8number;
+//        u8number--;
+//    }
+//
+//    printf("%d\n",u8factorial);
+//    return 0;
+//}
