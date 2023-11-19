ca5a9db7bc2c6182fedd8a7644046aff3786c9bf
Salah Bendary <salahbendary1@gmail.com>
2023-11-19 23:28:09 +0200

    Update Ex4.c

--- Assignment1/Ex4.c
+++ Assignment1/Ex4.c
@@ -6,19 +6,19 @@
 //calculates the area and circumference then prints the results.
 // **************************************************************************************************/
 //
-//#define PI 3.14
-//#include <stdio.h>
-//
-//int main () {
-//	setvbuf(stdout, NULL, _IONBF, 0);
-//	setvbuf(stderr, NULL, _IONBF, 0);
-//	unsigned int u32circuleRadius = 0;
-//	unsigned int u32circuleArea = 0;
-//	unsigned int u32circuleCircumference = 0;
-//	printf("Enter the Radius of the circle:\t");
-//	scanf("%d",&u32circuleRadius);
-//	u32circuleArea = PI*u32circuleRadius*u32circuleRadius;
-//	u32circuleCircumference = (PI*(u32circuleRadius+u32circuleRadius));
-//	printf("The area of the circle: %d\nAnd the circumference is: %d\n",u32circuleArea,u32circuleCircumference);
-//	return 0 ;
-//}
+#define PI 3.14
+#include <stdio.h>
+
+int main () {
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+	unsigned int u32circuleRadius = 0;
+	unsigned int u32circuleArea = 0;
+	unsigned int u32circuleCircumference = 0;
+	printf("Enter the Radius of the circle:\t");
+	scanf("%d",&u32circuleRadius);
+	u32circuleArea = PI*u32circuleRadius*u32circuleRadius;
+	u32circuleCircumference = (PI*(u32circuleRadius+u32circuleRadius));
+	printf("The area of the circle: %d\nAnd the circumference is: %d\n",u32circuleArea,u32circuleCircumference);
+	return 0 ;
+}
