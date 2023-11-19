847ebd1b523edb43909952ae13e66e18c2f29f56
Salah Bendary <salahbendary1@gmail.com>
2023-11-19 23:28:17 +0200

    Update Ex16.c

--- Assignment1/Ex16.c
+++ Assignment1/Ex16.c
@@ -5,22 +5,22 @@
 // Write a program to reverse a number.
 // **************************************************************************************************/
 //
-//#include <stdio.h>
-//
-//int main () {
-//    setvbuf(stdout, NULL, _IONBF, 0);
-//    setvbuf(stderr, NULL, _IONBF, 0);
-//    unsigned int u32reversedNumber = 0;
-//    unsigned int u32reminder =0;
-//    unsigned int u32number=1;
-//    printf("Enter the number:");
-//    scanf("%d",&u32number);
-//    printf("The Reversed Number is: %d",u32number);
-//    while(u32number!=0) {
-//    	u32reminder = u32number%10;
-//    	u32reversedNumber = u32reversedNumber * 10 + u32reminder;
-//    	u32number/=10;
-//    }
-//    printf(" After reversed is: %d",u32reversedNumber);
-//    return 0;
-//    }
+#include <stdio.h>
+
+int main () {
+ setvbuf(stdout, NULL, _IONBF, 0);
+ setvbuf(stderr, NULL, _IONBF, 0);
+ unsigned int u32reversedNumber = 0;
+ unsigned int u32reminder =0;
+ unsigned int u32number=1;
+ printf("Enter the number:");
+ scanf("%d",&u32number);
+ printf("The Reversed Number is: %d",u32number);
+ while(u32number!=0) {
+ 	u32reminder = u32number%10;
+ 	u32reversedNumber = u32reversedNumber * 10 + u32reminder;
+ 	u32number/=10;
+ }
+ printf(" After reversed is: %d",u32reversedNumber);
+ return 0;
+ }
