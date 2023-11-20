18bc9144844d377fb4ab0ada5e0e00a6ff5c6d00
Salah Bendary <salahbendary1@gmail.com>
2023-11-20 18:05:00 +0200

    Create Ex7.c

---
+++ Assignment2/Ex7.c
@@ -0,0 +1,26 @@
+///**************************************************************************************************
+// Name        : Ex7.c
+// Author      : Salah BENDARY
+// Description : Write a C Function that reads two integers and checks if the first is
+//multiple of the second.
+// **************************************************************************************************/
+//
+//#include <stdio.h>
+//#include "std_types.h"
+//
+//int isMultiple(uint8 u8NumberOne, uint8 u8NumberTwo) {
+//	uint8 u8result = 1;
+//	if(u8NumberOne*u8NumberOne==u8NumberTwo) { u8result = 1;}
+//	else { u8result = 0;}
+//	return u8result;
+//}
+//
+//int main () {
+//	uint8 u8NumberOne = 2;
+//	uint8 u8NumberTwo = 4;
+//	uint8 u8result;
+//	u8result = isMultiple(u8NumberOne,u8NumberTwo);
+//	if(u8result) { printf("is Multiple!!! ");}
+//	else {printf("Not a Multiple!!! "); }
+//	return 0;
+//}
