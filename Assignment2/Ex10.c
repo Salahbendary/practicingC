17678e4c167412db6987e1347d471fa4134c1d6e
Salah Bendary <salahbendary1@gmail.com>
2023-11-20 18:05:56 +0200

    Update Ex10.c

--- Assignment2/Ex10.c
+++ Assignment2/Ex10.c
@@ -10,28 +10,28 @@
 //Complete the function countHoles. The function must return an integer
 //denoting the total number of holes in num.
 // **************************************************************************************************/
-//
-//#include <stdio.h>
-//#include "std_types.h"
-//
-//int countHoles(uint16 u8firstNumb) {
-//	uint8 u8oneDigit;
-//	uint8 u8countHoles = 0;
-//	while(u8firstNumb!=0) {
-//		u8oneDigit = u8firstNumb%10;
-//		if (u8oneDigit == 8) {u8countHoles+=2;}
-//		else if((u8oneDigit == 0) || (u8oneDigit == 4) ||
-//				(u8oneDigit == 6) ||(u8oneDigit == 9)) {
-//			u8countHoles++;
-//		}
-//		u8firstNumb/=10;
-//	}
-//	return u8countHoles;
-//}
-//int main () {
-//	uint16 u8NumberOne = 819;
-//	uint8 u8result;
-//	u8result = countHoles(u8NumberOne);
-//	printf("Count of Holes = %d",u8result);
-//	return 0;
-//}
+
+#include <stdio.h>
+#include "std_types.h"
+
+int countHoles(uint16 u8firstNumb) {
+	uint8 u8oneDigit;
+	uint8 u8countHoles = 0;
+	while(u8firstNumb!=0) {
+		u8oneDigit = u8firstNumb%10;
+		if (u8oneDigit == 8) {u8countHoles+=2;}
+		else if((u8oneDigit == 0) || (u8oneDigit == 4) ||
+				(u8oneDigit == 6) ||(u8oneDigit == 9)) {
+			u8countHoles++;
+		}
+		u8firstNumb/=10;
+	}
+	return u8countHoles;
+}
+int main () {
+	uint16 u8NumberOne = 819;
+	uint8 u8result;
+	u8result = countHoles(u8NumberOne);
+	printf("Count of Holes = %d",u8result);
+	return 0;
+}
